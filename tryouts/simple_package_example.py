1c5bf5d076e974635854b9f094facff3b14cc8b4
girish pasupathy <girishpasupathy@gmail.com>
2020-04-28 08:39:47 +0530

    Added bandit and pydocstyle support
    * Removed mypy extension since official python extension supports mypy out of the box
    
    Changes to be committed:
    	modified:   .gitignore
    	modified:   .vscode/extensions.json
    	modified:   Pipfile
    	modified:   Pipfile.lock
    	modified:   README.md
    	modified:   python_learning.code-workspace
    	modified:   tryouts/simple_package_example.py

--- tryouts/simple_package_example.py
+++ tryouts/simple_package_example.py
@@ -6,3 +6,15 @@ from tryouts import simple_package
 
 simple_package.a.foo()
 simple_package.b.bar()
+
+
+def say_hello(name: str) -> None:
+    """Print hello to the console.
+
+    Arguments:
+        name {str} -- Name of the person to say hello to
+    """
+    print(f"Hello, {name}")
+
+
+say_hello("John")
